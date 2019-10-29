621284fde7a12bf24d650ce85e22d3a6a416d832
Angulito-15 <sala@HOR-SALA501-20.unihorizonte.salas>
2019-10-29 15:51:58 -0500

    Mi primer commit

---
+++ SenaSalus/src/senasalus/SenaSalus.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package senasalus;
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+public class SenaSalus {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nom;
+        Scanner nombre= new Scanner(System.in);
+        
+       System.out.println("Cual es tu nombre");
+       nom=nombre.next();
+       System.out.println(("Su nombre es:"+nom);
+       
+        
+        
+    }
+    
+}
