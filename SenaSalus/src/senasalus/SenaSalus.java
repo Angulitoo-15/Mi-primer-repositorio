ef6a4c6c4ead1e96c2943f30a22498117e0e0e53
Angulito-15 <sala@HOR-SALA501-20.unihorizonte.salas>
2019-10-29 16:58:23 -0500

    Mi tercer commit

--- SenaSalus/src/senasalus/SenaSalus.java
+++ SenaSalus/src/senasalus/SenaSalus.java
@@ -13,11 +13,11 @@ public class SenaSalus {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String apellido;
-           Scanner nombre = new Scanner(System.in);
-           System.out.println("Digite su apellido");
-           apellido=nombre.next();
-           System.out.println("su apellido es:"+apellido);
+        String Documento;
+           Scanner DI = new Scanner(System.in);
+           System.out.println("Digite su Documento");
+           Documento=DI.next();
+           System.out.println("su edad es:"+DI);
                    
        
        
